a89c7df9f0e78dfa51ab01035601b03c5e865954
afikmenashe21 <amttrt@gmail.com>
2020-08-23 19:41:56 +0300

    Add project files.

---
+++ MovieStore/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace MovieStore.Models
+{
+    public enum Genres
+    {
+        Action, Adventure, Animation, Biography,
+        Comedy, Crime, Documentary, Drama,
+        Family, Fantasy, History, Horror, Music,
+        Mystery, Romance, Sport, War
+    }
+
+    public class Movie
+    {
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public ICollection<string> Genre { get; set; }
+        public int Duration { get; set; }
+        // public Supplier supplier { get; set; }
+        public ICollection<string> Cast { get; set; }
+        public double Rate { get; set; }//Fetch from IMBD
+        public string Director { get; set; }
+
+    }
+}
