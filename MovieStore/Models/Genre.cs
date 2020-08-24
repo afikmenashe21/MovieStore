94387ee7643617f0ecf5f5511a840d9f5ded3595
afikmenashe21 <amttrt@gmail.com>
2020-08-24 16:02:39 +0300

    Added Models

---
+++ MovieStore/Models/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public enum GenreTypes
+{
+    Action, Adventure, Animation, Biography,
+    Comedy, Crime, Documentary, Drama,
+    Family, Fantasy, History, Horror, Music,
+    Mystery, Romance, Sport, War
+}
+
+namespace MovieStore.Models
+{
+
+    public class Genre
+    {
+
+        public int Id { get; set; }
+        virtual public ICollection<Movie> Movies { get; set; }
+        public string Type { get; set; }
+    }
+}
