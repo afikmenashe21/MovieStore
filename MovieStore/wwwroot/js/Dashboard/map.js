5dd2f89cfe22bb2f2e6b4303a0f4511e0eab6a24
afikmenashe21 <amttrt@gmail.com>
2020-11-01 13:59:14 +0200

    Changed from Google API to Bing Maps API, current commit with random pins

--- MovieStore/wwwroot/js/Dashboard/map.js
+++ MovieStore/wwwroot/js/Dashboard/map.js
@@ -1,9 +1,15 @@
-﻿var map;
+﻿var geocoder;
+var map;
+var address = "new york city";
+
 function initMap() {
+
     map = new google.maps.Map(document.getElementById('map-canvas'), {
         center: { lat: 50.641111, lng: 4.668056 },
         zoom: 7
     });
+    //geocoder = new google.maps.Geocoder();
+    //codeAddress(geocoder, map);
 
     var markerGrandPlace = new google.maps.Marker({
         position: new google.maps.LatLng(50.846759, 4.352446),
@@ -16,12 +22,12 @@ function initMap() {
         map.setZoom(20);
     });
 
-    var markerGrandPlace = new google.maps.Marker({
-        position: new google.maps.LatLng(50.846759, 4.352446),
-        map: map,
-        title: "Brussels Grand-Place",
-        icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-    });
+    //var markerGrandPlace = new google.maps.Marker({
+    //    position: new google.maps.LatLng(50.846759, 4.352446),
+    //    map: map,
+    //    title: "Brussels Grand-Place",
+    //    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+    //});
 }
 var responsiveZoom = (window.innerWidth < 768) ? 6.75 : 7.75;
 
@@ -30,3 +36,20 @@ window.addEventListener("resize", function () {
     else if (window.innerWidth > 768) responsiveZoom = 7.75
     map.setZoom(responsiveZoom);
 });
+
+//////////////////////
+
+
+function codeAddress(geocoder, map) {
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
