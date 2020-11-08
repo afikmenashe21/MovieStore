6c8d2fe5141061e53a5f51de1292266d4f7bce42
afikmenashe21 <amttrt@gmail.com>
2020-11-08 13:11:31 +0200

    Complete Bing maps

--- MovieStore/wwwroot/js/Dashboard/map.js
+++ MovieStore/wwwroot/js/Dashboard/map.js
@@ -1,55 +1,90 @@
-﻿var geocoder;
-var map;
-var address = "new york city";
+﻿var map, infobox;
+var usersAddress = [];
+var userNames = [];
 
-function initMap() {
+function GetMap() {
+    map = new Microsoft.Maps.Map('#map-canvas', {});
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-        center: { lat: 50.641111, lng: 4.668056 },
-        zoom: 7
-    });
-    //geocoder = new google.maps.Geocoder();
-    //codeAddress(geocoder, map);
-
-    var markerGrandPlace = new google.maps.Marker({
-        position: new google.maps.LatLng(50.846759, 4.352446),
-        map: map,
-        title: "Brussels Grand-Place"
+    //Create an infobox at the center of the map but don't show it.
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: false
     });
 
-    google.maps.event.addListener(markerGrandPlace, "click", function () {
-        map.panTo(this.getPosition());
-        map.setZoom(20);
-    });
+    //Assign the infobox to a map instance.
+    infobox.setMap(map);
 
-    //var markerGrandPlace = new google.maps.Marker({
-    //    position: new google.maps.LatLng(50.846759, 4.352446),
-    //    map: map,
-    //    title: "Brussels Grand-Place",
-    //    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-    //});
-}
-var responsiveZoom = (window.innerWidth < 768) ? 6.75 : 7.75;
-
-window.addEventListener("resize", function () {
-    if (window.innerWidth < 768) responsiveZoom = 6.75
-    else if (window.innerWidth > 768) responsiveZoom = 7.75
-    map.setZoom(responsiveZoom);
-});
-
-//////////////////////
-
-
-function codeAddress(geocoder, map) {
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status === 'OK') {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
+    //Get the data of the user, e.g address and name
+    var table = document.getElementById("addressTable");
+    for (let i = 0; i < table.children.length; i++) {
+        if (i % 2 === 0) {
+            usersAddress.push(table.children[i].textContent);
         } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            userNames.push(table.children[i].textContent);
         }
+
+    }
+
+    function getData(address, index) {
+        Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+            var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+            var requestOptions = {
+                bounds: map.getBounds(),
+                where: address,
+                callback: function (answer, userData) {
+                    map.entities.push(myPushPin(answer.results[0].location, index));
+                }
+            };
+            searchManager.geocode(requestOptions);
+        });
+    }
+
+    (async function markPins() {
+        try {
+            for (var i = 0; i < usersAddress.length; i++) {
+                var add = usersAddress[i].toString();
+                await getData(add, i);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+    })();
+
+    map.setView({
+        center: new Microsoft.Maps.Location(32.109333, 34.855499),
+        zoom: 8
     });
+}
+
+
+
+function myPushPin(location, index) {
+    var pin = new Microsoft.Maps.Pushpin(location);
+    var name = userNames[index].toString();
+    var loc = usersAddress[index].toString();
+
+    //Store some metadata with the pushpin.
+    pin.metadata = {
+        title: 'User:' + name,
+        description: 'Address: ' + loc
+    };
+
+    //Add a click event handler to the pushpin.
+    Microsoft.Maps.Events.addHandler(pin, 'click', pushpinClicked);
+
+    //Add pushpin to the map.
+    map.entities.push(pin);
+}
+
+function pushpinClicked(e) {
+    //Make sure the infobox has metadata to display.
+    if (e.target.metadata) {
+        //Set the infobox options with the metadata of the pushpin.
+        infobox.setOptions({
+            location: e.target.getLocation(),
+            title: e.target.metadata.title,
+            description: e.target.metadata.description,
+            visible: true
+        });
+    }
 }
