453334d378d4aa58cf18d4b0b1cc3f29733792d1
afikmenashe21 <amttrt@gmail.com>
2020-11-09 20:48:30 +0200

    Working version of tweet , CSS is missing

--- MovieStore/Controllers/MoviesController.cs
+++ MovieStore/Controllers/MoviesController.cs
@@ -688,14 +688,106 @@ namespace MovieStore.Controllers
         /*
          string movieName , string releaseDate , string movieDirector , int movieRating
          */
-        public void Tweet(string movieName, string releaseDate, string movieDirector, int movieRating)
+        //public void Tweet(string movieName, string releaseDate, string movieDirector, int movieRating)
+        //{
+        //    string twitterURL = "https://api.twitter.com/1.1/statuses/update.json";
+
+        //    string oauth_consumer_key = "ogrOneaqh9a1HXbUDflObM64N";
+        //    string oauth_consumer_secret = "iZEcLq1fR1Ps60N5G0yP3gcATniK870j1OeeGA82z72hU2i7ku";
+        //    string oauth_token = "1325414546182430721-5fgmOh2pKpy1ZLwkYT1oMjjaQy88w0";
+        //    string oauth_token_secret = "2UZ88dVVgHVorMcXylB4KaXDsXRW1r2srdlVRus2uPbA1";
+
+        //    // set the oauth version and signature method
+        //    string oauth_version = "1.0";
+        //    string oauth_signature_method = "HMAC-SHA1";
+
+        //    // create unique request details
+        //    string oauth_nonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+        //    System.TimeSpan timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+        //    string oauth_timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+
+        //    // create oauth signature
+        //    string baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" + "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&status={6}";
+
+
+        //    string messageToPost = "Hi There !\n" +
+        //                          "Someone just explored the movie " + movieName + "!\n" +
+        //                          "Release Date: " + releaseDate + ".\n" +
+        //                          "Director: " + movieDirector + ".\n" +
+        //                          "IMDB Rating: " + movieRating + ".\n" +
+        //                          "Whats a great Movie";
+
+        //    string baseString = string.Format(
+        //    baseFormat,
+        //    oauth_consumer_key,
+        //    oauth_nonce,
+        //    oauth_signature_method,
+        //    oauth_timestamp, oauth_token,
+        //    oauth_version,
+        //    Uri.EscapeDataString(messageToPost)
+        //);
+
+        //    string oauth_signature = null;
+        //    using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(Uri.EscapeDataString(oauth_consumer_secret) + "&" + Uri.EscapeDataString(oauth_token_secret))))
+        //    {
+        //        oauth_signature = Convert.ToBase64String(hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes("POST&" + Uri.EscapeDataString(twitterURL) + "&" + Uri.EscapeDataString(baseString))));
+        //    }
+
+        //    // create the request header
+        //    string authorizationFormat = "OAuth oauth_consumer_key=\"{0}\", oauth_nonce=\"{1}\", " + "oauth_signature=\"{2}\", oauth_signature_method=\"{3}\", " + "oauth_timestamp=\"{4}\", oauth_token=\"{5}\", " + "oauth_version=\"{6}\"";
+
+        //    string authorizationHeader = string.Format(
+        //        authorizationFormat,
+        //        Uri.EscapeDataString(oauth_consumer_key),
+        //        Uri.EscapeDataString(oauth_nonce),
+        //        Uri.EscapeDataString(oauth_signature),
+        //        Uri.EscapeDataString(oauth_signature_method),
+        //        Uri.EscapeDataString(oauth_timestamp),
+        //        Uri.EscapeDataString(oauth_token),
+        //        Uri.EscapeDataString(oauth_version)
+        //    );
+
+        //    HttpWebRequest objHttpWebRequest = (HttpWebRequest)WebRequest.Create(twitterURL);
+        //    objHttpWebRequest.Headers.Add("Authorization", authorizationHeader);
+        //    objHttpWebRequest.Method = "POST";
+        //    objHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+        //    using (Stream objStream = objHttpWebRequest.GetRequestStream())
+        //    {
+        //        byte[] content = ASCIIEncoding.ASCII.GetBytes("status=" + Uri.EscapeDataString(messageToPost));
+        //        objStream.Write(content, 0, content.Length);
+        //    }
+
+        //    var responseResult = "";
+
+        //    try
+        //    {
+        //        //success posting
+        //        WebResponse objWebResponse = objHttpWebRequest.GetResponse();
+        //        StreamReader objStreamReader = new StreamReader(objWebResponse.GetResponseStream());
+        //        responseResult = objStreamReader.ReadToEnd().ToString();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        responseResult = "Twitter Post Error: " + ex.Message.ToString() + ", authHeader: " + authorizationHeader;
+        //    }
+
+        //}
+
+        public void Tweet(string movieName, string releaseDate, string movieDirector, string movieRating)
         {
+            string messageToPost = "Hi There !\n" +
+                                 "Someone just explored the movie " + movieName + "!\n" +
+                                 "Release Date: " + releaseDate + ".\n" +
+                                 "Director: " + movieDirector + ".\n" +
+                                 "IMDB Rating: " + movieRating + ".\n" +
+                                 "Whats a great Movie";
+
             string twitterURL = "https://api.twitter.com/1.1/statuses/update.json";
 
-            string oauth_consumer_key = "0Uv01QshcLim4irHA9OcRU7vD";
-            string oauth_consumer_secret = "Ob2SlXFo3KiMfptKMAOqDNz6lVKpjifq08RYViJWQW0TKGpfAw";
-            string oauth_token = "1325414546182430721-2PEKxM3xbmAwmQ8XCJ85wd3QijyrU6";
-            string oauth_token_secret = "lY0FbrSAa1eEDIFAmORjD04kOfkK8lrcP9VZZJxSik3DF";
+            string oauth_consumer_key = "ogrOneaqh9a1HXbUDflObM64N";
+            string oauth_consumer_secret = "iZEcLq1fR1Ps60N5G0yP3gcATniK870j1OeeGA82z72hU2i7ku";
+            string oauth_token = "1325414546182430721-5fgmOh2pKpy1ZLwkYT1oMjjaQy88w0";
+            string oauth_token_secret = "2UZ88dVVgHVorMcXylB4KaXDsXRW1r2srdlVRus2uPbA1";
 
             // set the oauth version and signature method
             string oauth_version = "1.0";
@@ -709,23 +801,15 @@ namespace MovieStore.Controllers
             // create oauth signature
             string baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" + "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&status={6}";
 
-
-            string messageToPost = "Hi There !\n" +
-                                  "Someone just explored the movie " + movieName + "!\n" +
-                                  "Release Date: " + releaseDate + ".\n" +
-                                  "Director: " + movieDirector + ".\n" +
-                                  "IMDB Rating: " + movieRating + ".\n" +
-                                  "Whats a great Movie";
-
             string baseString = string.Format(
-            baseFormat,
-            oauth_consumer_key,
-            oauth_nonce,
-            oauth_signature_method,
-            oauth_timestamp, oauth_token,
-            oauth_version,
-            Uri.EscapeDataString(messageToPost)
-        );
+                baseFormat,
+                oauth_consumer_key,
+                oauth_nonce,
+                oauth_signature_method,
+                oauth_timestamp, oauth_token,
+                oauth_version,
+                Uri.EscapeDataString(messageToPost)
+            );
 
             string oauth_signature = null;
             using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(Uri.EscapeDataString(oauth_consumer_secret) + "&" + Uri.EscapeDataString(oauth_token_secret))))
